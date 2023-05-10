440f7acb04ef72feeff63b6d2f1c000cd4abac65
Escaper3 <2037063847@qq.com>
2023-05-10 04:12:08 -0400

    Testability: Unit test suite

--- tictactoe/test/TestExample.java
+++ tictactoe/test/TestExample.java
@@ -14,42 +14,46 @@ import controller.RowGameController;
  * An example test class, which merely shows how to write JUnit tests.
  */
 public class TestExample {
-	private RowGameController humanGame;
+	private RowGameController game;
+	///////////////////////
 	private RowGameController computerGame;
 
+	//////////////////////////
 	@Before
 	public void setUp() {
-		humanGame = new RowGameController();
+		game = new RowGameController();
+		////////////////
 		computerGame = new ComputerRowGameController();
+		/////////////////
 	}
 
 	@After
 	public void tearDown() {
-		humanGame = null;
+		game = null;
+		/////////////////
 		computerGame = null;
+		/////////////////
 	}
 
 	private void checkInitialConfiguration() {
 		// Check the post-conditions for the new RowGameModel
-		assertNotNull(humanGame.gameModel);
+		assertNotNull(game.gameModel);
 		// The first player has the initial move
-		assertEquals(Player.PLAYER_1, humanGame.gameModel.getPlayer());
+		assertEquals(Player.PLAYER_1, game.gameModel.getPlayer());
 		// There are initially 9 possible moves left to make
-		assertEquals(9, humanGame.gameModel.movesLeft);
+		assertEquals(9, game.gameModel.movesLeft);
 		// Each block is empty and a legal move
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col < 3; col++) {
-				assertEquals("", humanGame.gameModel.blocksData[row][col].getContents());
-				assertEquals(true, humanGame.gameModel.blocksData[row][col].getIsLegalMove());
+				assertEquals("", game.gameModel.blocksData[row][col].getContents());
+				assertEquals(true, game.gameModel.blocksData[row][col].getIsLegalMove());
 			} // end for col
 		} // end for row
 			// The final result is null
-		assertNull(humanGame.gameModel.getFinalResult());
+		assertNull(game.gameModel.getFinalResult());
 	}
 
-	// Against a computer player: After performing a legal move, the game is updated
-	// appropriately.
-
+	//
 	@Test
 	public void testNewGame() {
 		// The @Before method performs the setup and calls the unit under test.
@@ -72,39 +76,19 @@ public class TestExample {
 		// Check the pre-conditions (i.e. class invariants)
 		this.checkInitialConfiguration();
 		// Call the unit under test: Execute a legal move
-		humanGame.move(blockIndex);
+		game.move(blockIndex);
 		// Check the post-conditions (i.e. class invariants)
-		assertEquals(Player.PLAYER_2, humanGame.gameModel.getPlayer());
-		assertEquals(8, humanGame.gameModel.movesLeft);
-		assertEquals("X", humanGame.gameModel.blocksData[blockIndex.getRow()][blockIndex.getColumn()].getContents());
+		assertEquals(Player.PLAYER_2, game.gameModel.getPlayer());
+		assertEquals(8, game.gameModel.movesLeft);
+		assertEquals("X", game.gameModel.blocksData[blockIndex.getRow()][blockIndex.getColumn()].getContents());
 		assertEquals(false,
-				humanGame.gameModel.blocksData[blockIndex.getRow()][blockIndex.getColumn()].getIsLegalMove());
+				game.gameModel.blocksData[blockIndex.getRow()][blockIndex.getColumn()].getIsLegalMove());
 	}
 
 	@Test
 	public void testLegalMove() {
 		BlockIndex blockIndex = new BlockIndex(0, 0);
 		testLegalMoveHelper(blockIndex);
-	}
-
-	// Against a computer player: After performing a legal move, the game is updated
-	// appropriately.
-	@Test
-	public void testComputerUpdate() {
-		BlockIndex move = new BlockIndex(0, 0);
-		computerGame.move(move);
-		// ensure 0, 0 == X
-		assertEquals("X", computerGame.gameModel.blocksData[move.getRow()][move.getColumn()].getContents());
-		// ensure some other block is now O
-		boolean foundO = false;
-		for (int row = 0; row < 3 && !foundO; row++) {
-			for (int col = 0; col < 3 && !foundO; col++) {
-				if ("O".equals(computerGame.gameModel.blocksData[row][col].getContents())) {
-					foundO = true;
-				}
-			}
-		}
-		assertTrue(foundO);
 	}
 
 	@Test(expected = UnsupportedOperationException.class)
@@ -114,7 +98,7 @@ public class TestExample {
 		testLegalMoveHelper(blockIndex);
 
 		// Call the unit under test: Execute an illegal move
-		humanGame.move(blockIndex);
+		game.move(blockIndex);
 
 		// Check the post-conditions (i.e. class invariants).
 		// See the @Test annotation.
@@ -123,41 +107,41 @@ public class TestExample {
 	@Test
 	public void testGameOverWin() {
 		// Perform setup and check the pre-conditions
-		humanGame.move(new BlockIndex(0, 0));
-		humanGame.move(new BlockIndex(2, 2));
-		humanGame.move(new BlockIndex(0, 1));
-		humanGame.move(new BlockIndex(2, 1));
+		game.move(new BlockIndex(0, 0));
+		game.move(new BlockIndex(2, 2));
+		game.move(new BlockIndex(0, 1));
+		game.move(new BlockIndex(2, 1));
 		for (int column = 0; column < 2; column++) {
-			assertEquals("X", humanGame.gameModel.blocksData[0][column].getContents());
+			assertEquals("X", game.gameModel.blocksData[0][column].getContents());
 		} // end for column
-		assertEquals(null, humanGame.gameModel.getFinalResult());
+		assertEquals(null, game.gameModel.getFinalResult());
 		// Call the unit under test: Player 1 has 3 Xs in a row
 		BlockIndex player1WinsBlockIndex = new BlockIndex(0, 2);
-		humanGame.move(player1WinsBlockIndex);
+		game.move(player1WinsBlockIndex);
 		// Check the post-conditions
 		assertEquals("X",
-				humanGame.gameModel.blocksData[player1WinsBlockIndex.getRow()][player1WinsBlockIndex.getColumn()]
+				game.gameModel.blocksData[player1WinsBlockIndex.getRow()][player1WinsBlockIndex.getColumn()]
 						.getContents());
-		assertEquals("Player 1 wins!", humanGame.gameModel.getFinalResult());
+		assertEquals("Player 1 wins!", game.gameModel.getFinalResult());
 	}
 
 	@Test
 	public void testGameOverTie() {
 		// Perform setup and check the pre-conditions
 		for (int column = 0; column < 3; column++) {
-			humanGame.move(new BlockIndex(0, column));
+			game.move(new BlockIndex(0, column));
 		} // end for column
 		for (int column = 0; column < 3; column++) {
-			humanGame.move(new BlockIndex(2, column));
+			game.move(new BlockIndex(2, column));
 		} // end for column
 		for (int column = 0; column < 2; column++) {
-			humanGame.move(new BlockIndex(1, column));
+			game.move(new BlockIndex(1, column));
 		} // end for column
-		assertEquals(null, humanGame.gameModel.getFinalResult());
+		assertEquals(null, game.gameModel.getFinalResult());
 		// Call the unit under test: Neither player wins
-		humanGame.move(new BlockIndex(1, 2));
+		game.move(new BlockIndex(1, 2));
 		// Check the post-conditions
-		assertEquals(RowGameModel.GAME_END_NOWINNER, humanGame.gameModel.getFinalResult());
+		assertEquals(RowGameModel.GAME_END_NOWINNER, game.gameModel.getFinalResult());
 	}
 
 	@Test
@@ -167,7 +151,7 @@ public class TestExample {
 		testLegalMoveHelper(blockIndex);
 
 		// Call the unit under test: Execute reset
-		humanGame.resetGame();
+		game.resetGame();
 
 		// Check the post-conditions
 		this.checkInitialConfiguration();
@@ -179,7 +163,7 @@ public class TestExample {
 		this.checkInitialConfiguration();
 
 		// Call the unit under test: Execute undo
-		humanGame.undo();
+		game.undo();
 
 		// Check the post-conditions. See the @Test annotation.
 	}
@@ -191,9 +175,34 @@ public class TestExample {
 		testLegalMoveHelper(blockIndex);
 
 		// Call the unit under test: Execute undo
-		humanGame.undo();
+		game.undo();
 
 		// Check the post-conditions
 		this.checkInitialConfiguration();
 	}
+	////////////////////////////////////////////////////////////////////////
+	//////// 1. Against a computer player://////////////////////////////////
+	//////// After performing a legal move, the game is updated////////////
+	//////// appropriately./////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////
+
+	@Test
+	public void testComputerUpdate() {
+		boolean IsUpdate = false;
+		// make a legal move
+		BlockIndex move = new BlockIndex(0, 0);
+		computerGame.move(move);
+		assertEquals("X", computerGame.gameModel.blocksData[move.getRow()][move.getColumn()].getContents());
+
+		///// travesal/////////////
+		for (int row = 0; row < 3 && !IsUpdate; row++) {
+			for (int col = 0; col < 3 && !IsUpdate; col++) {
+				if ("O".equals(computerGame.gameModel.blocksData[row][col].getContents())) {
+					IsUpdate = true;
+				}
+			}
+		}
+		assertTrue(IsUpdate);
+	}
+
 }
